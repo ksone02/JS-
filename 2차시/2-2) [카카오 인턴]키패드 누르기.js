aa98de9f1ee8cbaaf5a4ba0dcfa6296368fc7b01
ksone02 <eksovus@gmail.com>
2021-07-07 17:04:15 +0900

    Update 2-2) [카카오 인턴]키패드 누르기.js

---
+++ 2차시/2-2) [카카오 인턴]키패드 누르기.js
@@ -0,0 +1,15 @@
+function solution(numbers, hand) {
+    var key = [[1,2,3],
+               [4,5,6],
+               [7,8,9],
+               ['*',0,'#']];
+    var leftF = key[3][0];
+    var rightF = key[3][2];
+    
+    for(var i=0;i<numbers.length;i++) {
+        if(numbers[i])
+    }
+    
+    var answer = leftF;
+    return answer;
+}
