08fa68601f589903bb346f4036d2d4e09ad0ac63
ksone02 <eksovus@gmail.com>
2021-07-11 22:41:20 +0900

    Update 3-3) 위장.js

--- 3차시/3-3) 위장.js
+++ 3차시/3-3) 위장.js
@@ -4,14 +4,14 @@ function solution(clothes) {
     var k = clothes.map(e => e.pop());
     
     if(k.length != 1) {
-        var counted = k.reduce(function (allNames, name) {
-          if (name in allNames) {
-            allNames[name]++;
+        var counted = k.reduce(function (all, el) {
+          if (el in all) {
+            all[el]++;
           }
           else {
-            allNames[name] = 1;
+            all[el] = 1;
           }
-          return allNames;
+          return all;
         }, {});
 
         obj = Object.values(counted).reduce((m,n) => m*n);
@@ -22,19 +22,19 @@ function solution(clothes) {
 
 
 function solution(clothes) {
-    var k = clothes.map(e => e.pop());
+    var k = clothes.map(e => e.pop()); //의상종류만
     
-    var counted = k.reduce(function (allNames, name) {
-          if (name in allNames) {
-            allNames[name]++;
+    var counted = k.reduce(function (all, el) {
+          if (el in all) {
+            all[el]++;
           }
           else {
-            allNames[name] = 1;
+            all[el] = 1;
           }
-          return allNames;
-    }, {});
+          return all;
+    }, {}); //객체 개수 리턴 {:2,:1...}
     
-    var obj = Object.values(counted).map(e => e+1).reduce((m,n) => m*n);
+    var obj = Object.values(counted).map(e => e+1).reduce((m,n) => m*n); //(겹쳐입는경우 + 혼자입는경우) * 나머지
     
-    return obj - 1;
+    return obj - 1; //하나만입는경우
 }
