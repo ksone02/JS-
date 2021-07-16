b3d311ddde0985a04ba34912b8722e821cc7de05
ksone02 <eksovus@gmail.com>
2021-07-16 17:06:15 +0900

    Update 5-1) 스킬트리.js

--- 5차시/5-1) 스킬트리.js
+++ 5차시/5-1) 스킬트리.js
@@ -1,10 +1,14 @@
 function solution(skill, skill_trees) {
-    let newArr = skill.split("");
-    let str = 0;
-    let count = 0;
-    for(let i=0;i<skill_trees.length;i++){
-        str = skill_trees[i].split("").filter(element => newArr.includes(element)).join("");
-        if(str === skill.substring(0,str.length)) count++;
+    var sk = skill.split(""); //"C","B","D"
+    var answer = 0;
+    
+    for(var i = 0; i < skill_trees.length; i++) {
+        //skill_trees를 스킬 하나하나 나눠주고, 그 중 CBD만 추출
+        //jshint ignore:start
+        var newStr = skill_trees[i].split("").filter((el) => sk.includes(el)).join("");
+        //jshint ignore:end
+        //스킬 순서가 동일하다면 카운트 + 1
+        if(newStr === skill.substring(0, newStr.length)) answer++;
     }
-    return count; 
+    return answer;
 }
